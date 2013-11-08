352c4ea08c82f546a3cee348bec294a3c307034f
meganai <meganai.com@gmail.com>
2013-11-08 14:44:16 +0900

    css

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,8 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
-    respond_to do |format|
+      @articles = Article.all
+      respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @articles }
     end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ActiveRecord::Base
-  attr_accessible :content, :qualification_id, :title, :user_id
-  has_many :responses, dependent: :destroy
-  belongs_to :qualification
-  belongs_to :user
+
+	attr_accessible :content, :qualification_id, :title, :user_id
+	has_many :responses, dependent: :destroy
+	belongs_to :qualification
+	belongs_to :user
+
+	validates_presence_of :title
 end
